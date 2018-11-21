11ae3a746f79c87243c90d95e807995da1223171
bimal <linkbimal123@gmail.com>
2018-11-21 18:07:53 +0545

    #2 primitive state:  added heroes list and detail section

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Spider Man'},
+  {id: 12, name: 'Super Man'},
+  {id: 13, name: 'Iron Man'},
+  {id: 14, name: 'Bat Man'},
+  {id: 15, name: 'Ant Man'}
+
+];
+
